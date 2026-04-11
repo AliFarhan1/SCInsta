6413bba98a318264fbc7ea0a8ad28276bae537fc
AliFarhan1 <152177280+AliFarhan1@users.noreply.github.com>
2026-04-11 11:26:43 +0300

    Update SCISymbol.m

--- src/Settings/SCISymbol.m
+++ src/Settings/SCISymbol.m
@@ -23,64 +23,56 @@
     if (self) {
         self.name = @"";
         self.color = [UIColor labelColor];
-        self.weight = UIImageSymbolWeightRegular;
-        self.size = 15.0;
+        self.weight = UIImageSymbolWeightMedium;
+        self.size = 18.0;
     }
     
     return self;
 }
 
 - (UIImage *)image {
-    UIImage *symbol = [UIImage systemImageNamed:self.name];
+    UIImageSymbolConfiguration *symbolConfig =
+    [UIImageSymbolConfiguration configurationWithPointSize:self.size
+                                                   weight:self.weight
+                                                    scale:UIImageSymbolScaleMedium];
     
-    if (self.size || (self.size && self.weight)) {
-        UIImageSymbolConfiguration *symbolConfig = [UIImageSymbolConfiguration configurationWithTextStyle:UIFontTextStyleTitle1];
-        symbolConfig = [symbolConfig configurationByApplyingConfiguration:
-                        [UIImageSymbolConfiguration configurationWithPointSize:self.size weight:self.weight]];
-        
-        return [symbol imageWithConfiguration:symbolConfig];
+    UIImage *symbol = [UIImage systemImageNamed:self.name withConfiguration:symbolConfig];
+    if (!symbol) {
+        return [UIImage new];
     }
     
-    return symbol;
+    return [symbol imageWithTintColor:self.color renderingMode:UIImageRenderingModeAlwaysOriginal];
 }
 
 // MARK: - Class methods
 
 + (instancetype)symbolWithName:(NSString *)name {
     SCISymbol *symbol = [[self alloc] init];
-    
     symbol.name = name;
-
     return symbol;
 }
 
 + (instancetype)symbolWithName:(NSString *)name color:(UIColor *)color {
     SCISymbol *symbol = [[self alloc] init];
-    
     symbol.name = name;
-    symbol.color = color;
-
+    symbol.color = color ?: [UIColor labelColor];
     return symbol;
 }
 
 + (instancetype)symbolWithName:(NSString *)name color:(UIColor *)color size:(CGFloat)size {
     SCISymbol *symbol = [[self alloc] init];
-    
     symbol.name = name;
-    symbol.color = color;
-    symbol.size = size;
-    
+    symbol.color = color ?: [UIColor labelColor];
+    symbol.size = size > 0 ? size : 18.0;
     return symbol;
 }
 
 + (instancetype)symbolWithName:(NSString *)name color:(UIColor *)color size:(CGFloat)size weight:(UIImageSymbolWeight)weight {
     SCISymbol *symbol = [[self alloc] init];
-    
     symbol.name = name;
-    symbol.color = color;
-    symbol.size = size;
+    symbol.color = color ?: [UIColor labelColor];
+    symbol.size = size > 0 ? size : 18.0;
     symbol.weight = weight;
-    
     return symbol;
 }
 
