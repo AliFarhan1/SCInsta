4c1b4ccc0a97988f44f37ca09d065ac8a492633f
AliFarhan1 <152177280+AliFarhan1@users.noreply.github.com>
2026-04-11 11:26:00 +0300

    Update SCISymbol.h

--- src/Settings/SCISymbol.h
+++ src/Settings/SCISymbol.h
@@ -1,21 +1,57 @@
-#import <UIKit/UIKit.h>
+#import "SCISymbol.h"
 
-NS_ASSUME_NONNULL_BEGIN
+@interface SCISymbol ()
 
-@interface SCISymbol : NSObject
-
-@property (nonatomic, copy, readonly) NSString *name;
-@property (nonatomic, copy, readonly) UIColor *color;
-@property (nonatomic, readonly) CGFloat size;
-@property (nonatomic, readonly) UIImageSymbolWeight weight;
-
-- (UIImage *)image;
-
-+ (instancetype)symbolWithName:(NSString *)name;
-+ (instancetype)symbolWithName:(NSString *)name color:(UIColor *)color;
-+ (instancetype)symbolWithName:(NSString *)name color:(UIColor *)color size:(CGFloat)size;
-+ (instancetype)symbolWithName:(NSString *)name color:(UIColor *)color size:(CGFloat)size weight:(UIImageSymbolWeight)weight;
+@property (nonatomic, copy, readwrite) NSString *name;
+@property (nonatomic, copy, readwrite) UIColor *color;
+@property (nonatomic, readwrite) CGFloat size;
+@property (nonatomic, readwrite) UIImageSymbolWeight weight;
 
 @end
 
-NS_ASSUME_NONNULL_END
+@implementation SCISymbol
+
++ (instancetype)symbolWithName:(NSString *)name {
+    return [self symbolWithName:name
+                          color:[UIColor labelColor]
+                           size:18.0
+                         weight:UIImageSymbolWeightMedium];
+}
+
++ (instancetype)symbolWithName:(NSString *)name color:(UIColor *)color {
+    return [self symbolWithName:name
+                          color:color
+                           size:18.0
+                         weight:UIImageSymbolWeightMedium];
+}
+
++ (instancetype)symbolWithName:(NSString *)name color:(UIColor *)color size:(CGFloat)size {
+    return [self symbolWithName:name
+                          color:color
+                           size:size
+                         weight:UIImageSymbolWeightMedium];
+}
+
++ (instancetype)symbolWithName:(NSString *)name color:(UIColor *)color size:(CGFloat)size weight:(UIImageSymbolWeight)weight {
+    SCISymbol *symbol = [[SCISymbol alloc] init];
+    symbol.name = name;
+    symbol.color = color ?: [UIColor labelColor];
+    symbol.size = size > 0 ? size : 18.0;
+    symbol.weight = weight;
+    return symbol;
+}
+
+- (UIImage *)image {
+    UIImageSymbolConfiguration *config = [UIImageSymbolConfiguration configurationWithPointSize:self.size
+                                                                                         weight:self.weight
+                                                                                          scale:UIImageSymbolScaleMedium];
+
+    UIImage *image = [UIImage systemImageNamed:self.name withConfiguration:config];
+    if (!image) {
+        return [UIImage new];
+    }
+
+    return [image imageWithTintColor:self.color renderingMode:UIImageRenderingModeAlwaysOriginal];
+}
+
+@end
